e7d2df935a4878f4a6f0bb0ecaf9f6d1847e2e39
Abid Habib ✅ <41414028+abidhabib@users.noreply.github.com>
2023-06-26 18:45:16 +0500

    pic added

--- src/components/Helpform.jsx
+++ src/components/Helpform.jsx
@@ -2,7 +2,7 @@ export const Header = () => {
     return (
       <div className="relative">
         <img
-          src="https://images.pexels.com/photos/3228766/pexels-photo-3228766.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+          src="https://i.ibb.co/NYj3mh1/ppp.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
           className="absolute inset-0 object-cover w-full h-full"
           alt=""
         />
